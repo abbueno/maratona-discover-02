39d96f39fd38f1f2871278bace2b1485bddcc827
ALLAN BIGHI BUENO <allanbbueno@bol.com.br>
2021-05-14 15:30:35 -0300

    part4-ok
    part4-ok

--- src/db/init.js
+++ src/db/init.js
@@ -1,64 +1,74 @@
-const Database = require('config')
+const Database = require('./config')
 
-Database()
+const initDb = {
+   async init(){         
 
-Database.exec(`CREATE TABLE profile (
+const db = await Database()
+
+await db.exec(`CREATE TABLE profile (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    avatar TEXT,
-    monthly_budget INT,
+    name TEXT, 
+    avatar TEXT, 
+    monthly_budget INT, 
     days_per_week INT,
     hours_per_day INT,
     vacation_per_year INT,
     value_hour INT
 )`);
 
-Database.exec(`CREATE TABLE jobs (
+await db.exec(`CREATE TABLE jobs (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
+    name TEXT, 
     daily_hours INT,
     total_hours INT,
-    created_at DATETIME    
-)`);
+    created_at DATETIME
+)`)
 
-Database.run(`INSERT INTO profile (
-    name,
-    avatar,
-    monthly_budget,
-    days_per_week,
-    hours_per_day,
+await db.run(`INSERT INTO profile (
+    name, 
+    avatar, 
+    monthly_budget, 
+    days_per_week, 
+    hours_per_day, 
     vacation_per_year,
-) VALUES (
-    "Allan",
-    https://avatars.githubusercontent.com/u/71529943?s=60&v=4,
-    3000,
-    5,
-    5,
-    4,
+    value_hour
+ ) VALUES (
+     "jakeliny",
+     "https://avatars.githubusercontent.com/u/17316392",
+     3000,
+     5,
+     5,
+     4,
+     70
 );`)
 
-Database.run(`INSERT INTO jobs (
-    name,
+await db.run(`INSERT INTO jobs (
+    name, 
     daily_hours,
     total_hours,
-    created_at,
+    created_at
 ) VALUES (
     "Pizzaria Guloso",
     2,
     1,
-    1617514376018    
+    1617514376018
 );`)
 
-Database.run(`INSERT INTO jobs (
-    name,
+await db.run(`INSERT INTO jobs (
+    name, 
     daily_hours,
     total_hours,
-    created_at,
+    created_at
 ) VALUES (
     "OneTwo Projects",
     3,
-    7,
-    1617514376018    
+    47,
+    1617514376018
 );`)
 
-Database.close()
+await db.close()
+    }
+}
+
+
+initDb.init()
